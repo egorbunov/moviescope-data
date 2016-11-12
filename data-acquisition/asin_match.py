2732d896f21d614261052d2de83f154b568ff3a2
Egor Gorbunov <egor-mailbox@ya.ru>
2016-11-12 20:37:43 +0300

    Fix some

---
+++ data-acquisition/asin_match.py
@@ -0,0 +1,35 @@
+import moviescope_db
+import sys
+import json
+
+
+def get_all_titles(connection):
+    cursor = connection.cursor()
+    cursor.execute("SELECT wiki_id, title FROM movie")
+    return dict([(row[0], row[1]) for row in cursor])
+
+
+def read_asins_and_data(file):
+    data_arr = json.load(file)
+    return [(o['asin'], o['title']) for o in data_arr]
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("USAGE: python p/t/script <DB_CONFIG_FILE.json> <ASIN_DATA.json>")
+        sys.exit(1)
+
+    with open(sys.argv[1]) as f:
+        conn = moviescope_db.connect_db(*moviescope_db.read_db_config(f))
+
+    with open(sys.argv[2]) as asin_data:
+        asins_data = read_asins_and_data(asin_data)
+
+    known_titles_dict = get_all_titles(conn)
+    known_titles = set(known_titles_dict.values())
+    asin_match_cnt = 0
+    for (asin, title) in asins_data:
+        if title in known_titles:
+            asin_match_cnt += 1
+
+    print("Got {} asin matches.".format(asin_match_cnt))
+
--- imdb_crawl/imdb_getter.py
+++ imdb_crawl/imdb_getter.py
@@ -74,6 +74,3 @@ if __name__ == "__main__":
 
     conn.close()
 
-
-
-
